4380bce2e863f1188b3de35d465e53654329483b
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-12-10 12:30:45 +0000

    Added timer

--- Day9_2.cpp
+++ Day9_2.cpp
@@ -1,15 +1,15 @@
 #include <algorithm>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-
 using namespace std;
 
 unordered_map<int, int> joltsFound;
 vector<long long> cache;
-long long solve(vector<int>& jolts, const int& joltsSize, const int start) {
+long long solve(vector<int>& jolts, const int& joltsSize, const int& start) {
     if (start > joltsSize) {
         return 0;
     }
@@ -40,6 +40,8 @@ int main() {
     vector<int> jolts;
 
     unordered_map<int, int> dp;
+    std::chrono::steady_clock::time_point begin =
+        std::chrono::steady_clock::now();
     while (!f.eof()) {
         int jolt;
         f >> jolt;
@@ -51,6 +53,7 @@ int main() {
     for (int i = 0; i < joltsSize; ++i) {
         joltsFound.insert({jolts[i], i});
     }
+
     long long res = 0;
     res += solve(jolts, joltsSize, 0);
     if (jolts[1] <= 3) {
@@ -59,5 +62,11 @@ int main() {
     if (jolts[2] <= 3) {
         res += solve(jolts, joltsSize, 2);
     }
-    cout << res << '\n';
+    std::chrono::steady_clock::time_point end =
+        std::chrono::steady_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::microseconds>(end - begin)
+            .count() /
+        1000000.0;
+    cout << res << " " << duration << '\n';
 }
