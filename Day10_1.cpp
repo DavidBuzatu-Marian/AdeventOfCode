b98ab08ec2d54e3bc0f08b9415aec00e0decfa54
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-12-11 09:01:57 +0000

    (main) Added solution for day 11, problem 1

---
+++ Day10_1.cpp
@@ -0,0 +1,66 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<int> directionsI = {-1, -1, -1, 0, 0, 1, 1, 1};
+vector<int> directionsJ = {-1, 0, 1, -1, 1, -1, 0, 1};
+int inBounds(const int &i, const int &j, const int &N, const int &M) {
+    return (i >= 0 && i < N) && (j >= 0 && j < M);
+}
+
+int checkAdjacency(vector<vector<char>> &matrix, const int &i, const int &j,
+                   const char &check) {
+    int cnt = 0;
+    for (int k = 0; k < directionsI.size(); ++k) {
+        int newI = i + directionsI[k];
+        int newJ = j + directionsJ[k];
+        if (inBounds(newI, newJ, matrix.size(), matrix[0].size()) &&
+            matrix[newI][newJ] == check) {
+            ++cnt;
+        }
+    }
+    return cnt;
+}
+
+int BFS(vector<vector<char>> &matrixOfSeats) {
+    vector<vector<char>> newMatrixOfSeats(matrixOfSeats);
+    int changed = 0;
+    for (int i = 0; i < matrixOfSeats.size(); ++i) {
+        for (int j = 0; j < matrixOfSeats[0].size(); ++j) {
+            if (matrixOfSeats[i][j] == 'L' &&
+                checkAdjacency(matrixOfSeats, i, j, '#') == 0) {
+                newMatrixOfSeats[i][j] = '#';
+                changed = 1;
+            } else if (matrixOfSeats[i][j] == '#' &&
+                       checkAdjacency(matrixOfSeats, i, j, '#') > 3) {
+                newMatrixOfSeats[i][j] = 'L';
+                changed = 1;
+            }
+        }
+    }
+    matrixOfSeats = newMatrixOfSeats;
+    return changed;
+}
+
+int main() {
+    ifstream f("day10_1.txt");
+    vector<vector<char>> matrixOfSeats;
+    while (!f.eof()) {
+        string line;
+        f >> line;
+        vector<char> lineChar(line.begin(), line.end());
+        matrixOfSeats.push_back(lineChar);
+    }
+    int ans = 0;
+    while (BFS(matrixOfSeats) == 1)
+        ;
+    for (int i = 0; i < matrixOfSeats.size(); ++i) {
+        for (int j = 0; j < matrixOfSeats[0].size(); ++j) {
+            if (matrixOfSeats[i][j] == '#') {
+                ++ans;
+            }
+        }
+    }
+    cout << ans << '\n';
+}
